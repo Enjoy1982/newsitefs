325940d914dca34df02b7c14e281ff218d26b59a
adri <136649939+Enjoy1982@users.noreply.github.com>
2025-02-27 00:23:22 +0100

    Update sql_NEWSITEV2.sql

--- server_configs/sql_NEWSITEV2.sql
+++ server_configs/sql_NEWSITEV2.sql
@@ -78,6 +78,21 @@ CREATE TABLE `addon_inventory_items` (
   `owner` varchar(60) DEFAULT NULL
 ) ENGINE=InnoDB;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing`
+--
+
+CREATE TABLE `billing` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(60) NOT NULL,
+  `sender` varchar(60) NOT NULL,
+  `target_type` varchar(50) NOT NULL,
+  `target` varchar(40) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
